0ae902a27aa97fd274e2e82f178fe055dea43e12
byteforge <yazen@alsaho.de>
2026-08-30 02:18:54 +0200

    Add Revolut transaction endpoint

---
+++ alltag-mobile/api/revolut/transactions.js
@@ -0,0 +1,8 @@
+import { handleBankTransactions } from '../_lib/bank-transactions.js';
+
+export default function handler(req, res) {
+  return handleBankTransactions(req, res, {
+    cookieName: 'alltag_revolut',
+    provider: 'revolut'
+  });
+}
